42bb1b91f6b60087527b2bfa49278a25f4042689
huseyin <hbayrak.hbayrak@gmail.com>
2020-10-29 14:04:44 +0200

    first

---
+++ src/Main.js
@@ -0,0 +1,28 @@
+ 
+import React from 'react';
+import { SafeAreaView ,View,Button} from 'react-native';
+
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+// import {reducer, initialState } from './context';
+import initialState from './context/store';
+import reducer from './context/reducer';
+
+import Component_A from './pages/Component_A';
+import Component_B from './pages/Component_B';
+
+const store = createStore(reducer, initialState);
+
+const Main = (props) => {
+    return (
+        <Provider store={store}>
+            <SafeAreaView style={{ flex: 1 }}>
+                <Component_A />
+                <Component_B />
+            </SafeAreaView>
+        </Provider>
+    );
+};
+
+export default Main;
